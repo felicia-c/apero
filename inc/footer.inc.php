926691391bf51ac732ba8167d4ef296239202135
Felicia Cuneo <felicia.cuneo@yahoo.fr>
2016-01-17 18:45:15 +0100

    ajout logo

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -2,9 +2,14 @@
 	
 	echo '</div>
 	<footer class="text-center">
-		<a href="'.RACINE_SITE.'contact.php">contact</a> | 
-		<a href="'.RACINE_SITE.'inscription_newsletter.php">newsletter</a>
-		<br /><br /><br />
+	<br />
+	<br />
+  	<p class="text-center" ><img src="'.RACINE_SITE.'images/bouteilles_apero.png" alt="apéro" width="100px" class="text-center"/></p>
+  	<br />
+  	<a href="'.RACINE_SITE.'contact.php">contact</a> | 
+	<a href="'.RACINE_SITE.'inscription_newsletter.php">newsletter</a>
+	<br />
+ 	<br />
 	</footer>';
 	echo '</div>
 		</div>
