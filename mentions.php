8be0c432fbf34fc0c782590e979ab17b508c01fc
Felicia Cuneo <felicia.cuneo@yahoo.fr>
2016-02-01 21:36:33 +0100

    ajout mentions légales

---
+++ mentions.php
@@ -0,0 +1,46 @@
+<?php
+require_once("inc/init.inc.php");
+$titre_page = "Mentions Légales";
+//APERO - Felicia Cuneo 02/2015
+require_once("inc/header.inc.php");
+
+?>
+<div class="box_info" >
+	<p><a href="" class="" onClick="(window.history.back())" title="page précédente"> < Retour</a></p>
+	<br />
+	<p>
+    Ce site fictif a été réalisé dans le cadre de la formation Développeur Intégrateur Web de <strong>l'institut IFOCOP</strong> pour la session DIW33.</p>
+	<h4>Site réalisé par: <a href="" alt="">Felicia Cuneo</a></h4>
+	
+	<h4>Article 1. Propriété intellectuelle :</h4>
+	<p class="text-justify">L'ensemble de ce site relève des législations Françaises et Internationales sur les droits d'auteur et la propriété intellectuelle.
+		Tous les droits de reproduction sont réservés pour les textes et les photographies de ce site.
+		La reproduction de tout ou partie de ce site sur un support électronique ou autre quel qu'il soit, est formellement interdite sauf autorisation écrite de l'auteur, conformément à l'article L 122-4 du Code de la Propriété intellectuelle.</p>
+	<h4>Article 2. Conditions de modération des commentaires :</h4>
+	<p class="text-justify" >Les commentaires sur les salles sont modérés a posteriori.<br>
+		Le non respect de l'une des règles suivantes entraîne la suppression du commentaire concerné :</p>
+	<ul>
+		<li>Commentaires diffamatoires, racistes, pornographiques, pédophiles, incitant à délit, crimes ou suicides,</li>
+		<li>Commentaires reproduisant une correspondance privée sans l'accord des personnes concernées,</li>
+		<li>Commentaires agressifs ou vulgaires,</li>
+		<li>Commentaires visant uniquement à mettre en ligne un lien vers un site extérieur (spam de commentaires ou de trackback/pingback).</li>
+	</ul>
+	<h4>Article 3. Liens hypertextes :</h4>
+	<p class="text-justify" >Les liens hypertextes mis en place dans le cadre du présent site internet en direction d'autres ressources présentes sur le réseau Internet, et notamment vers ses partenaires ont fait l'objet d'une autorisation préalable, expresse et écrite auprès de l'éditeur.</p>
+
+	<h4>Article 4. Droit de réponse :</h4>
+	<p class="text-justify" >Toute personne citée sur le site peut faire valoir un droit de réponse. Une simple demande sera adressée par courrier.</p>
+
+	<h4>Article 5. Politique de confidentialité :</h4>
+	<p class="text-justify" >En aucun cas, les données recueillies sur le site ne seront cédées ou vendues à des tiers. Aucune adresse email ne sera transmise à des tiers y compris à nos partenaires sauf avec l'accord écrit des intéressés.</p>
+
+	<h4>Article 6. Conditions générales de vente :</h4>
+	<p class="text-justify" >Vous retrouverez les conditions générales de vente à la page suivante : <a href="" alt="" >Conditions générales de vente</a></p>
+</div>
+
+<br /><br />
+
+<?php
+require_once("inc/footer.inc.php");
+  
+?>
