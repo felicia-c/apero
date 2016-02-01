ad6a6ff594ec238a305cb46db505e0e073fa264d
Felicia Cuneo <felicia.cuneo@yahoo.fr>
2016-02-01 23:51:55 +0100

    ajout page a propos

---
+++ apero.php
@@ -0,0 +1,30 @@
+<?php
+require_once("inc/init.inc.php");
+$titre_page = "C'est quoi ?";
+//APERO - Felicia Cuneo - 02/2015
+require_once("inc/header.inc.php");
+?>
+<div class="box_info" >
+<h3>La marque</h3>
+<p><strong>Apéro</strong> est une marque de <strong>t-shirts sérigraphiés</strong>, réalisés par un atelier d’impression Parisien. 
+La marque étend aujourd’hui son activité sur le web en développant un concept original.
+</p>
+
+<h3>Le concept</h3>
+<p>Apéro met en place un <strong>partenariat avec les bars</strong>, leur permettant de développer et fidéliser leur clientèle.</p>
+<p>Les clients du site Apéro bénéficient de promotions dans les bars partenaires quand il portent leur T-shirt Apéro.</p>
+
+<h3>C'est-à-dire ?</h3>
+<p>En parallèle de son site e-commerce, Apéro crée un partenariat exclusif avec les bars:
+<br /><br />Les gérants peuvent créer une « page » pour leur bar, en renseignant les informations qu’ils souhaitent voir apparaître.
+<br /><br />Une fois les vérifications effectuées par notre équipe, la page est validée et visible par tous les utilisateurs du site.
+<br /><br />Chaque bar peut ensuite choisir de proposer des <strong>offres spéciales pour les clients du site.</strong></p>
+
+<h3>C'est quoi l'avantage pour les bars ?</h3>
+<p><strong>Les bars gagnent en visibilité</strong> et en notoriété, ils développent et fidélisent leur clientèle, <strong>en gardant la maîtrise totale des promotions qu’ils proposent</strong> sur le site. </p>
+
+</div>
+<br />
+<br />
+<?php
+require_once("inc/footer.inc.php");
