71513027b2113a6ede06edbab57dbd84f98b935d
Felicia Cuneo <felicia.cuneo@yahoo.fr>
2016-02-01 22:49:33 +0100

    ajout cgv

---
+++ cgv.php
@@ -0,0 +1,55 @@
+<?php
+require_once("inc/init.inc.php");
+$titre_page = "Conditions Générales de Vente";
+//APERO - Felicia Cuneo - 02/2015
+require_once("inc/header.inc.php");
+?>
+<div class="box_info" >
+	<p><a href="" class="" onClick="(window.history.back())" title="page précédente"> < Retour</a></p>
+   <br />
+   <br />
+
+	<p>Ce site FICTIF a été réalisé dans le cadre de l'atelier PHP prévu lors de la formation Développeur Intégrateur Web de <strong>l'institut IFOCOP</strong> pour la session DIW33. Toutes les informations qui y figurent sont factices.</p> 
+	<h4>Site réalisé par: <a href="http://wwww.feliciacuneo.com" alt="">Felicia Cuneo</a></h4>
+	
+    <h4>Article 1 : Objet</h4>
+    <p>Les présentes conditions régissent la vente des produits présentés sur ce site par la société : APERO XXXX <br />
+    Téléphone : ++ 33X XX XX XX XX
+    <br />e-mail : XXXXX@XXXX.com
+    <br />La commande implique l'adhésion aux conditions générales de vente. Toute commande ne sera prise en considération qu'après acceptation du GIE Cartes Bancaires, interrogé via le service de paiements sécurisés CyberPlus Paiements de la BANQUE POPULAIRE DU NORD. Les parties conviennent que le contrat est soumis au droit Français.
+    Les produits sont offerts à la vente depuis la France ou l'étranger, livrables en France métropolitaine et dans toute l'Europe.
+    Pour les livraisons à l'étranger, veuillez appeler notre service de commandes par téléphone au ++ 33X XX XX XX XX. Seuls les produits disponibles peuvent être commandés. Concernant les articles en rupture de stock, vous pouvez demander à être informé(e) de leur disponibilité par e-mail. Les photographies illustrant à l'appui du texte les produits,
+    n'entrent pas dans le champ contractuel. L'offre est valable jusqu'à épuisement des stocks.
+    <br />
+    Les prix sont présentés en Euros, la TVA incluse est la TVA Française. <br />
+    Identification TVA : FRXXXXXXXXXX</p>
+
+    <h4>Article 2 : Droits d'auteur et propriété intellectuelle</h4>
+    <p>Le titre, la conception, la forme du site Apéro mais aussi son contenu sont la propriété de Apéro. Toute utilisation d'éléments du site Apéro, y compris la reproduction, la modification, la diffusion ou la re-publication, sans l'autorisation écrite préalable de Apéro, est strictement interdite et constitutive d'une contrefaçon sanctionnée pénalement.</p>
+
+    <h4>Article 3 : Limites de responsabilité</h4>
+    <p>Le contenu de ce site peut comporter des erreurs et/ou des inexactitudes et/ou des omissions techniques et typographiques, ce que vous reconnaissez et acceptez en utilisant ce site. Les informations contenues dans le site sont non-contractuelles et sujettes à modification sans préavis.
+    <br />Les informations présentées sur le site sont régulièrement mises à jour. De ce fait, Apéro ne garantit pas que les fonctions ou certaines pages du site seront en ligne et assurées sans interruption et sans erreur,
+     ni que le site ou le serveur qui les mets à disposition sont exempts de virus et autres composants dangereux. En aucun cas, Apéro pourra être tenue responsable des préjudices fortuits,
+     directs ou indirects résultant de l'utilisation des éléments du site.
+
+    <br />Ce site Web peut fournir des liens ou des références à d'autres sites non contrôlés par Apéro. En conséquence, Apéro décline toute responsabilité quant aux contenus de ces autres sites et ne saurait être tenue responsable de tous dommages-intérêts ou dommages découlant de l'utilisation ou recours à de tels contenus ni de l'accès ou de l'impossibilité d'accès à ces sites.</p>
+
+    <h4>Article 4 : Informatique et liberté</h4>
+    <p>Conformément à la loi Informatique et Libertés du 6 janvier 1978, vous disposez auprès de Checkpoint d'un droit d'accès, de rectification ou de suppression concernant les données personnelles que nous pourrions être amenés à recueillir.
+        Ces données sont celles que vous aurez volontairement enregistrées lors de vos demandes de renseignements en ligne. Aucune information personnelle vous concernant n'est cédée à des tiers ou utilisée à des fins non prévues.</p>
+
+    <h4>Article 11 : acceptation des conditions de vente.</h4>
+    <p>Cette acceptation est liée à la signature du bon de réservation.</p>
+
+    <h4>Article 12 : contestation.</h4>
+    <p>Toute contestation relative aux présentes prestations relève de la seule compétence des tribunaux du ressort de la Cour d’appel de Paris.</p>
+    <h4>Developpement et hébergement</h4>
+    <p>Felicia Cuneo - contact @ feliciacuneo.com </p>
+</div>
+<br />
+<br />
+<?php
+require_once("inc/footer.inc.php");
+  
+?>
