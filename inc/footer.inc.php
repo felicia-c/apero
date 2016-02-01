edbd072717257df304691850f55c9b131baefa39
Felicia Cuneo <felicia.cuneo@yahoo.fr>
2016-02-01 21:37:40 +0100

    ajout liens ML, CGV, newsletter

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -2,11 +2,14 @@
 	
 	echo '</div>
 	<footer class="text-center">
-	<br />
+
 	<br />
   	<p class="text-center" ><img src="'.RACINE_SITE.'images/bouteilles_apero.png" alt="apéro" width="100px" class="text-center"/></p>
   	<br />
   	<a href="'.RACINE_SITE.'contact.php">contact</a> | 
+  	<a href="'.RACINE_SITE.'cgv.php">CGV</a> | 
+  	<a href="'.RACINE_SITE.'mentions.php">mentions légales</a> | 
+  	<a href="'.RACINE_SITE.'apero.php">apéro ?</a> | 
 	<a href="'.RACINE_SITE.'inscription_newsletter.php">newsletter</a>
 	<br />
  	<br />
@@ -14,4 +17,5 @@
 	echo '</div>
 		</div>
 	</body>
+
 </html>';
