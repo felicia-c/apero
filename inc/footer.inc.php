1b029f613a64243451500e1826b252f0f1e04ee0
Felicia Cuneo <felicia.cuneo@yahoo.fr>
2016-02-28 20:06:15 +0100

    ajout liens

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -7,12 +7,14 @@
   	<p class="text-center" ><img src="'.RACINE_SITE.'images/bouteilles_apero.png" alt="apéro" width="100px" class="text-center"/></p>
   	<br />
   	<a href="'.RACINE_SITE.'contact.php">contact</a> | 
-  	<a href="'.RACINE_SITE.'cgv.php">CGV</a> | 
+  	<a href="'.RACINE_SITE.'cgv.php">CGV</a> |   	
   	<a href="'.RACINE_SITE.'mentions.php">mentions légales</a> | 
-  	<a href="'.RACINE_SITE.'apero.php">apéro ?</a> | 
-	<a href="'.RACINE_SITE.'inscription_newsletter.php">newsletter</a>
+  	<a href="'.RACINE_SITE.'sitemap.php">plan</a> | 
+  	<a href="'.RACINE_SITE.'inscription_newsletter.php">newsletter</a> | 
+	<a href="'.RACINE_SITE.'apero.php">apéro ?</a>
 	<br />
  	<br />
+ 	<p class="ml_footer">L\'abus d\'alcool est dangeureux pour la santé</p>
 	</footer>';
 	echo '</div>
 		</div>
