5121e66f8dd3b2933096675c587a0f1ec52b591e
Felicia Cuneo <felicia.cuneo@yahoo.fr>
2016-02-28 20:01:35 +0100

    ajout page map

---
+++ sitemap.php
@@ -0,0 +1,40 @@
+<?php 
+require_once("inc/init.inc.php");
+//APERO  - Felicia Cuneo - 02/2016
+$titre_page = "Apéro - plan du site"; 
+
+require_once("inc/header.inc.php"); 
+?>
+<div id="box_info">
+
+
+
+<table cellpadding="0" cellspacing="0" border="0" width="100%">
+
+<tr><td><a href="http://feliciacuneo.com/apero/index.php" title="Accueil">Accueil</a></td></tr>
+<tr><td><a href="http://feliciacuneo.com/apero/boutique.php" title="T-shirts">T-shirts (Boutique)</a></td></tr>
+<tr><td><a href="http://feliciacuneo.com/apero/bars_et_promos.php" title="Bars">Bars</a></td></tr>
+<tr><td><a href="http://feliciacuneo.com/apero/panier.php" title="Panier">Panier</a></td></tr>
+<tr><td><a href="http://feliciacuneo.com/apero/inscription.php" title="Inscription">Inscription</a></td></tr>
+<tr><td><a href="http://feliciacuneo.com/apero/connexion.php" title="connexion">Connexion</a></td></tr>
+<tr><td><a href="http://feliciacuneo.com/apero/contact.php" title="Contact">Contact</a></td></tr>
+<tr><td><a href="http://feliciacuneo.com/apero/cgv.php" title="Conditions Générales de vente">Conditions Générales de Ventes</a></td></tr>
+<tr><td><a href="http://feliciacuneo.com/apero/mentions.php" title="Mentions Légales">Mentions Légales</a></td></tr>
+<tr><td><a href="http://feliciacuneo.com/apero/inscription_newsletter.php" title="Newsletter">Inscription Newsletter</a></td></tr>
+<tr><td><a href="http://feliciacuneo.com/apero/mdp.php" title="Mot de passe">Mot de passe oublié</a></td></tr>
+<tr><td colspan="99"><a style="color: silver;" target="_blank" href="https://www.xml-sitemaps.com">Google XML sitemap and html sitemaps generator</a> | Copyright &copy; 2005-2015</td></tr>
+</table>
+
+
+<!--
+Please note:
+You are not allowed to remove the copyright notice below.
+Thank you!
+www.xml-sitemaps.com
+-->
+
+
+
+<?php 
+require_once("inc/footer.inc.php");
+
